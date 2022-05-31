27d7a20d4a9a72604bfa128f29382af002ecd1d4
Zamir Kanji <zamirkanji@comcast.net>
2022-05-31 09:45:57 -0700

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+export default function Card (props) {
+    return (
+        <div className="card-container">
+            <div className='card-grid'>
+
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header () {
+    return (
+        <header className="App-header">
+            <h1>Memory Game!</h1>
+            <p>Try not to click the same picture twice</p>
+            <div>
+                {/* counter */}
+            </div>
+        </header>
+    )
+}
